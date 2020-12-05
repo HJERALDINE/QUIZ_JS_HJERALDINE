c5621b50529d64d6a8a227af6a15042b29ebf2f5
HJERALDINE <hjeraldine@misena.edu.co>
2020-12-04 21:55:13 -0500

    Primer ejercicio Js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 console.log ("Quiz Js Hjeraldine Ricaurte.js");
 
+/*Ejercicio No. 01*/
+var tipo_motor = 5
+
+switch (tipo_motor) {
+    case 0: 
+        document.write ("0. No hay establecido un valor definido para el tipo de bomba")
+        break;
+    case 1: 
+        document.write ("1. La bomba es una bomba de agua");
+        break;
+    case 2: 
+        document.write ("2. La bomba es una bomba de gasolina");
+        break;
+    case 3: 
+        document.write ("3. La bomba es una bomba de hormigón");
+        break;
+    case 4: 
+        document.write ("4. La bomba es una bomba de pasta alimenticia");
+        break;
+    default:
+        document.write ("5. No existe un valor válido para tipo de bomba");
+    }
